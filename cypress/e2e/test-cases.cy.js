9cb02ac5f1445836ef5455e8d254bc562a5e4c32
Pedro Campagnoli <littleozuka@gmail.com>
2026-04-21 23:33:53 -0300

    Criando UserFactory

---
+++ cypress/e2e/test-cases.cy.js
@@ -0,0 +1,15 @@
+import { createUser } from "../support/factories/UserFactory";
+
+describe("Test Cases", () => {
+  it("Test Case 1: Register User", () => {
+    const UserData = createUser();
+    cy.goTo("/");
+    cy.register(UserData);
+    cy.deleteAccount(UserData.name);
+  });
+  it("Test Case 2: Login User with correct email and password", () => {
+    const UserData = createUser();
+    cy.goTo("/");
+    cy.register(UserData);
+  });
+});
